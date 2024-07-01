8d8ffae04528cc6b7144f6dab127d442996cd529
EpaphrasSam <isinesam@gmail.com>
2024-07-01 10:44:39 +0000

    Initialized project with tailwindcss and tamaugui

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,39 @@
 import "../global.css";
+import "@tamagui/core/reset.css";
+import { useEffect, useState } from "react";
 import { Slot } from "expo-router";
+import { TamaguiProvider } from "tamagui";
+import { useFonts } from "expo-font";
+import tamaguiConfig from "tamagui.config";
+import { StatusBar } from "react-native";
+
+const StackLayout = () => {
+  return <Slot />;
+};
 
 export default function Layout() {
-  return <Slot />;
+  const [fontsLoaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    const prepareApp = async () => {
+      if (fontsLoaded) {
+        setAppIsReady(true);
+      }
+    };
+
+    prepareApp();
+  }, [fontsLoaded]);
+
+  if (!appIsReady) {
+    return null;
+  }
+  return (
+    <TamaguiProvider config={tamaguiConfig}>
+      <StackLayout />
+    </TamaguiProvider>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Button } from "tamagui";
 
 export default function Page() {
   return (
